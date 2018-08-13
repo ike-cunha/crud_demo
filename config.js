a64e18b969c5a4abdae6a71c54ed9df34bcbb385
Henrique Cunha <henrique.eccunha@gmail.com>
2018-08-12 22:28:24 -0300

    #001
    Commit inicial - configuracao e inicializacao dos modulos

---
+++ config.js
@@ -0,0 +1,15 @@
+var config = {
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        port: 8889,
+        db: 'musico'
+    },
+    server: {
+        host: '127.0.0.1',
+        port: '3000'
+    }
+}
+
+module.exports = config
